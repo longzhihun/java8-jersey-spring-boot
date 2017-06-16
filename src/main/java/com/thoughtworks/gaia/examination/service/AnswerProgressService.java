6554580c449261383dbc3d1927736fdad392c529
Xu Jin <devxu@CNdevxu.local>
2017-06-16 14:31:31 +0800

    add answer progress service

---
+++ src/main/java/com/thoughtworks/gaia/examination/service/AnswerProgressService.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.gaia.examination.service;
+
+import com.thoughtworks.gaia.examination.entity.Examination;
+
+/**
+ * Created by devxu on 2017/6/16.
+ */
+public class AnswerProgressService {
+
+    public Examination getAnswerProgress(Long examinationId) {
+        ExaminationModel productModel = examinationRepository.findOne(examinationId);
+        if (productModel == null) {
+            error("Examination not found with id: " + examinationId);
+            throw new NotFoundException();
+        }
+
+        return mapper.map(productModel, Product.class);
+    }
+}
