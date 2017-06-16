17e2883960116c2f4d6130c75a07e0d673f52f62
jlguo <38651850@qq.com>
2017-06-16 14:30:24 +0800

    update id type to Long.

--- src/main/java/com/thoughtworks/gaia/examination/endpoint/ExaminationEndPoint.java
+++ src/main/java/com/thoughtworks/gaia/examination/endpoint/ExaminationEndPoint.java
@@ -1,7 +1,39 @@
 package com.thoughtworks.gaia.examination.endpoint;
 
+import com.thoughtworks.gaia.examination.entity.Examination;
+import com.thoughtworks.gaia.product.entity.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by jlguo on 15/06/2017.
  */
+
+@Component
+@Path("examinations")
+@Api(value = "examination", description = "Access to examination resource")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ExaminationEndPoint {
+
+    @Path("/{examinationId}")
+    @ApiOperation(value = "Get examination by id", response = Examination.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get examination successfully"),
+            @ApiResponse(code = 404, message = "No examination matches given id")
+    })
+    @GET
+    public Response produceExamination(@PathParam("examinationId") Long examinationId) {
+
+        Examination e = new Examination();
+
+        return Response.ok().entity(e).build();
+    }
 }
--- src/main/java/com/thoughtworks/gaia/examination/service/ExaminationService.java
+++ src/main/java/com/thoughtworks/gaia/examination/service/ExaminationService.java
@@ -1,7 +1,15 @@
 package com.thoughtworks.gaia.examination.service;
 
+import com.thoughtworks.gaia.common.Loggable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by jlguo on 16/06/2017.
  */
-public class ExaminationService {
+@Service
+@Transactional
+public class ExaminationService implements Loggable {
+
+
 }
