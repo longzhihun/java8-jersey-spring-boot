ec5b4795bc5ea69bbf03e7bcfe60fab52b3cf7f7
jlguo <38651850@qq.com>
2017-06-16 15:04:44 +0800

    add examination endpoint to framework.

---
+++ src/main/java/com/thoughtworks/gaia/examination/ExaminationMapper.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.gaia.examination;
+
+import com.thoughtworks.gaia.common.MapperBase;
+
+/**
+ * Created by jlguo on 16/06/2017.
+ */
+public class ExaminationMapper extends MapperBase {
+}
--- src/main/java/com/thoughtworks/gaia/examination/service/ExaminationService.java
+++ src/main/java/com/thoughtworks/gaia/examination/service/ExaminationService.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.gaia.examination.service;
 
 import com.thoughtworks.gaia.common.Loggable;
+import com.thoughtworks.gaia.examination.entity.Examination;
+import com.thoughtworks.gaia.product.entity.Product;
+import com.thoughtworks.gaia.product.model.ProductModel;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * Created by jlguo on 16/06/2017.
@@ -11,5 +16,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ExaminationService implements Loggable {
 
+    public Examination create(Examination examination) {
+        Examination e = new Examination();
+        e.setCodingEndTime(new Date());
+        e.setId(110l);
+        e.setCodingNum(2);
+        e.setCodingLimitTime(500l);
+        return e;
+    }
 
 }
